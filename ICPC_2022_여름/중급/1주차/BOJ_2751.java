1fdec40044363d705b4acaa12f3ce65bbaf7fe8e
Yerin <yeerin99@gmail.com>
2022-07-11 21:29:51 +0900

    중급 1주차

---
+++ ICPC_2022_여름/중급/1주차/BOJ_2751.java
@@ -0,0 +1,26 @@
+package 중급_1주차;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2751 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N=Integer.parseInt(br.readLine());
+		List<Integer> list=new ArrayList<>();
+		
+		for(int i=0;i<N;i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(list);
+		
+		for(Integer i : list) {
+			sb.append(i+"\n");
+		}
+		
+		System.out.println(sb.toString().trim());
+	}
+}
