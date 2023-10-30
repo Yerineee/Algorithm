cb775b8878ea71134288777eb3ecf1d437c9ed26
Yerin Kim <87409442+Yerineee@users.noreply.github.com>
2023-10-30 17:15:29 +0900

    백준_조건문

---
+++ CODE/JS/BOJ_2884.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let line = fs.readFileSync('dev/stdin').toString().split('\n');
+
+let input = line[0].split(' ');
+let H = parseInt(input[0]);
+let M = parseInt(input[1]);
+
+if(M<45) {
+    H-=1;
+    if(H<0) H+=24;
+    M+=60;
+}
+M-=45;
+
+console.log(H+' '+M);
