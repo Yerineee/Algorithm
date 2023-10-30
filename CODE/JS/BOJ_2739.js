7f8d2ce61a239a8f3a5a090febf4e73bc2a5bdf5
Yerin Kim <87409442+Yerineee@users.noreply.github.com>
2023-10-30 17:38:13 +0900

    백준_구구단_반복문

--- CODE/JS/BOJ_2739.js
+++ CODE/JS/BOJ_2739.js
@@ -4,5 +4,6 @@ let input = fs.readFileSync('dev/stdin').toString().split('\n');
 let N = Number(input[0]);
 
 for(let i=1;i<=9;i++) {
-    console.log(N+" * "+i+" = "+N*i);
+    console.log(`${N} * ${i} = ${N*i}`);
+    // console.log(N+" * "+i+" = "+N*i);
 }
