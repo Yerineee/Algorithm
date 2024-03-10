7b39dc60f12c44f08dedfb13ffe963d784e32309
Yerin Kim <87409442+Yerineee@users.noreply.github.com>
2024-03-10 18:31:23 +0900

    BOJ_어두운 굴다리_구현

---
+++ CODE/BOJ_17266.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_17266 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());	// 굴다리의 길이 n
+		int m = Integer.parseInt(br.readLine());	// 가로등의 개수 m
+		int[] x = new int[m+1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		// 가로등의 위치 x
+		for(int i=1;i<=m;i++) {
+			x[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		/* 가로등 최소 높이가 될 수 있는 경우의 수 (아래 경우 들 중 최댓값)
+		 * 1. 첫 번재 가로등 위치와 0(굴다리 시작 위치) 사이의 거리
+		 * 2. 마지막 가로등 위치와 n(굴다리 끝 위치) 사이의 거리
+		 * 3. (가로등 사이의 거리)/2 => (가로등 개수가 1개보다 많은 경우)
+		 */
+		
+		x[0] = x[1];	// 1번 경우
+		// (가로등 개수가 1개보다 많다면) 3번 경우
+		if(m>1) {
+			for(int i=1;i<m;i++) {
+				x[i] = (int)Math.ceil((double)(x[i+1]-x[i])/2);
+			}
+		}
+		x[m] = n-x[m];	// 2번 경우
+		
+		Arrays.sort(x);	// 배열 x 오름차순 정렬
+		System.out.println(x[m]);	// 배열 x의 최댓값 출력 (가로등 최소 높이)
+	}
+}
