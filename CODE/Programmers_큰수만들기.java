6e237ffefa955b948bd76cba284ce7719afd1b1c
Yerin Kim <87409442+Yerineee@users.noreply.github.com>
2024-04-06 17:33:27 +0900

    Programmers_큰수만들기_그리디,스택

--- CODE/Programmers_큰수만들기.java
+++ CODE/Programmers_큰수만들기.java
@@ -25,3 +25,37 @@ class Solution {
         return answer.toString();
     }
 }
+
+/*
+// 스택을 사용한 풀이법
+import java.lang.StringBuilder;
+import java.util.Stack;
+
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        
+        Stack<Character> stack = new Stack<>();
+        
+        int restLen = number.length()-k;    // 남겨야 할 숫자 개수
+        
+        for(int i=0;i<number.length();i++) {
+            char c = number.charAt(i);  // 해당 자리의 숫자
+            
+            // 스택이 비어있지 않고, 해당 자리의 숫자가 스택 최상단 숫자보다 크고, k가 0보다 크다면 스택 최상단 숫자 꺼내기
+            while(!stack.isEmpty() && stack.peek()<c && k-->0) {
+                stack.pop();
+            }
+            
+            stack.push(c);  // 스택에 해당 자리의 숫자 저장
+        }
+    
+        // 남겨야 할 숫자 개수만큼 answer에 저장
+        for(int i=0;i<restLen;i++) {
+            answer.append(stack.get(i));
+        }
+        
+        return answer.toString();
+    }
+}
+*/
